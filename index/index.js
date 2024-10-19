b6f1ee5f0d972d91664093049f988b08e65ade3b
Yuexin <yuexinli1203@gmail.com>
2024-10-18 21:42:35 -0700

    wireframe imgs

--- index/index.js
+++ index/index.js
@@ -143,23 +143,23 @@ document.addEventListener("DOMContentLoaded", function () {
     //testimonial slide
     const testArr = [
         {
-            img: './images/wireframe1.png',
+            img: 'https://1233198063.github.io/Connect-with-a-animal-companion//images/wireframe1.png',
             client: 'WireFrame1'
         },
         {
-            img: './images/wireframe2.png',
+            img: 'https://1233198063.github.io/Connect-with-a-animal-companion//images/wireframe2.png',
             client: 'WireFrame2'
         },
         {
-            img: './images/wireframe3.png',
+            img: 'https://1233198063.github.io/Connect-with-a-animal-companion//images/wireframe3.png',
             client: 'WireFrame3'
         },
         {
-            img: './images/wireframe4.png',
+            img: 'https://1233198063.github.io/Connect-with-a-animal-companion//images/wireframe4.png',
             client: 'WireFrame4'
         },
         {
-            img: './images/wireframe5.png',
+            img: 'https://1233198063.github.io/Connect-with-a-animal-companion//images/wireframe5.png',
             client: 'WireFrame5'
         }
     ];
